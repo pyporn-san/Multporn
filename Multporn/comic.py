8361cdf0beac1c6a456c990ee10e58549dce5a46
pyporn-san <pypornsan@gmail.com>
2020-12-22 17:18:31 +0330

    stuff

--- Multporn/comic.py
+++ Multporn/comic.py
@@ -1,44 +1,162 @@
 import mimetypes
 import os
 from pathlib import Path
+from typing import List, Tuple
+from urllib.parse import urljoin, urlparse
+from urllib.request import getproxies
 
 import certifi
 import requests
 from bs4 import BeautifulSoup
+from faker import Faker
 from pathvalidate import sanitize_filepath
+from requests import HTTPError, Session
+from requests.adapters import HTTPAdapter
+from requests.models import Response
+from requests_html import HTMLSession
+from urllib3.util.retry import Retry
 
 
-class Comic:
-    def __init__(self, url: str, parentTag: str = "p", parentClass: str = "jb-image", download=False):
-        self.session = requests.sessions.Session()
-        self.session.verify = certifi.where()
-        self.home = "http://multporn.net/"
-        self.url = requests.compat.urljoin(self.home, url)
-        soup = BeautifulSoup(requests.get(self.url).text, "html.parser")
-        self.imageUrls = [image.find("img")["src"]
-                          for image in soup.find_all(parentTag, parentClass)]
-        # Loading tags
-        try:
-            self.tags = [i.next.text for i in soup.find(
-                text="Tags: ").find_next().contents]
-            self.ongoing = "ongoing" in soup.find(
-                text="Section: ").find_next().text.lower()
-        except AttributeError:
-            self.tags = None
-        # Support for older pages that used og:title
-        try:
-            self.name = sanitize_filepath(
-                soup.find("meta", attrs={"name": "dcterms.title"})["content"])
-        except TypeError:
-            self.name = sanitize_filepath(
-                soup.find("meta", attrs={"property": "og:title"})["content"])
+class RequestHandler(object):
+    """
+    RequestHandler
+    ==============
+    Defines a synchronous request handler class that provides methods and 
+    properties for working with REST APIs that is backed by the `requests`
+    library.
+    """
+    _timeout = (5, 5)
+    _total = 5
+    _status_forcelist = [413, 429, 500, 502, 503, 504]
+    _backoff_factor = 1
+    _fake = Faker()
+
+    def __init__(self,
+                 timeout: Tuple[float, float] = _timeout,
+                 total: int = _total,
+                 status_forcelist: List[int] = _status_forcelist,
+                 backoff_factor: int = _backoff_factor):
+        """
+        Instantiates a new request handler object.
+        """
+        self.timeout = timeout
+        self.total = total
+        self.status_forcelist = status_forcelist
+        self.backoff_factor = backoff_factor
+
+    @property
+    def retry_strategy(self) -> Retry:
+        """
+        The retry strategy returns the retry configuration made up of the
+        number of total retries, the status forcelist as well as the backoff
+        factor. It is used in the session property where these values are 
+        passed to the HTTPAdapter. 
+        """
+        return Retry(total=self.total,
+                     status_forcelist=self.status_forcelist,
+                     backoff_factor=self.backoff_factor
+                     )
+
+    @property
+    def session(self) -> Session:
+        """
+        Creates a custom session object. A request session provides cookie
+        persistence, connection-pooling, and further configuration options
+        that are exposed in the RequestHandler methods in form of parameters 
+        and keyword arguments.
+        """
+        assert_status_hook = lambda response, * \
+            args, **kwargs: response.raise_for_status()
+        session = requests.Session()
+        session.mount("https://", HTTPAdapter(max_retries=self.retry_strategy))
+        session.hooks['response'] = [assert_status_hook]
+        session.headers.update({
+            "User-Agent": RequestHandler._fake.chrome(version_from=80, version_to=86, build_from=4100, build_to=4200)
+        })
+        return session
+
+    def get(self, url: str, params: dict = None, **kwargs) -> Response:
+        """
+        Returns the GET request encoded in `utf-8`. Adds proxies to this session 
+        on the fly if urllib is able to pick up the system's proxy settings.
+        """
+        response = self.session.get(
+            url, timeout=self.timeout, params=params, proxies=getproxies(), **kwargs)
+        response.encoding = 'utf-8'
+        return response
+
+
+class Multporn(RequestHandler):
+    HOME = "http://multporn.net/"
+
+    def __init__(self, url: str, download: bool = False, timeout: Tuple[float, float] = RequestHandler._timeout,
+                 total: int = RequestHandler._total,
+                 status_forcelist: List[int] = RequestHandler._status_forcelist,
+                 backoff_factor: int = RequestHandler._backoff_factor):
+        """
+        Start a request session and load soup from <https://multporn.net> for this link.
+        """
+        super().__init__(timeout, total, status_forcelist, backoff_factor)
+        self.__handler = RequestHandler(
+            self.timeout, self.total, self.status_forcelist, self.backoff_factor)
+        self.__url = urljoin(self.HOME, url)
+        self.__response = self.__handler.get(self.__url)
+        self.__soup = BeautifulSoup(self.__response.text, "html.parser")
         if(download):
             self.downlaodImages(self)
+
+    @property
+    def imageUrls(self) -> list[str]:
+        """
+        Return the url of every image in the comic
+        """
+        imageUrls = [image.find("img")["src"]
+                     for image in self.__soup.find_all("p", "jb-image")]
+        return imageUrls
+
+    @property
+    def tags(self) -> list[str]:
+        try:
+            tags = [i.next.text for i in self.__soup.find(
+                text="Tags: ").find_next().contents]
+        except AttributeError:
+            tags = None
+        return tags
+
+    @property
+    def ongoing(self) -> bool:
+        """
+        Returns true if the comic is ongoing
+        """
+        try:
+            ongoing = "ongoing" in self.__soup.find(
+                text="Section: ").find_next().text.lower()
+        except AttributeError:
+            ongoing = False
+        return ongoing
+
+    @property
+    def name(self):
+        """
+        Returns the name of the comic
+        Some older comics had the name in a different location
+        """
+        try:
+            name = sanitize_filepath(
+                self.__soup.find("meta", attrs={"name": "dcterms.title"})["content"])
+        except TypeError:
+            name = sanitize_filepath(
+                self.__soup.find("meta", attrs={"property": "og:title"})["content"])
+        return name
 
     def __str__(self):
         return self.name
 
     def downlaodImages(self, output: bool = True, root: Path = Path("Comics/"), printProgress: bool = True):
+        """
+        Downloads all comic pages that don't already exist in the directory
+        logging can be disabled by passing false to printProgress
+        """
         Updated = 0
         existingStart = -1
         existingEnd = -1
@@ -77,24 +195,39 @@ class Comic:
                     print(f'"{self.name}" page {i+1}/{len(self.imageUrls)} done')
         if(printProgress):
             if(not Updated):
-                print(f'Downlaod "{self.url}" finished with no updates')
+                print(f'Downlaod "{self.__url}" finished with no updates')
             else:
                 print(
-                    f'Download of "{self.url}" done, {Updated} new pages found')
+                    f'Download of "{self.__url}" done, {Updated} new pages found')
 
 
-class Utils:
-    def fromWebpage(self, url):
-        links = []
-        global root
-        url = input("The webpage to load links from : ")
-        parsed = urlparse(url)
-        soup = BeautifulSoup(requests.get(url, verify=False).text, "html.parser")
-        root = sanitize_filepath(soup.find("meta", property="og:title")["content"])
-        try:
-            os.mkdir(f"Comics/{root}")
-        except:
-            pass
-        for i in soup.find("table", "views-view-grid").find_all("strong"):
-            links.append(f"{parsed[0]}://{parsed[1]}{i.a['href']}")
+class Webpage:
+    def __init__(self, url):
+        self.__url = url
+        self.__soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+    @property
+    def links(self) -> list:
+        """
+        return all links found in this webpage
+        """
+        links = [urljoin(Multporn.HOME, i.a['href']) for i in self.__soup.find(
+            "table", "views-view-grid").find_all("strong")]
         return links
+
+    @property
+    def name(self) -> str:
+        """
+        Return the name of this webpage
+        usually is a category, character, author, etc 
+        """
+        try:
+            self.__name = sanitize_filepath(
+                self.__soup.find("meta", attrs={"name": "dcterms.title"})["content"])
+        except TypeError:
+            self.__name = sanitize_filepath(
+                self.__soup.find("meta", attrs={"property": "og:title"})["content"])
+        return self.__name
+
+
+print(Webpage("https://multporn.net/comics/kim_possible"))
