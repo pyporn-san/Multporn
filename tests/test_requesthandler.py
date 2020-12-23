ca96e5273055e928ab26cb1973a6fa1b704cced8
pyporn-san <76444728+pyporn-san@users.noreply.github.com>
2020-12-23 21:52:35 +0330

    Create test_requesthandler.py

---
+++ tests/test_requesthandler.py
@@ -0,0 +1,14 @@
+import unittest
+
+from hentai import RequestHandler
+
+
+class TestRequestHandler(unittest.TestCase):
+    def test_call_api(self):
+        response = RequestHandler().get(url="https://nhentai.net/api/galleries/all")
+        self.assertTrue(response.ok)
+        self.assertNotIn('error', response.json())
+        
+
+if __name__ == '__main__':
+    unittest.main()
