f9e061c2f12fd8cae07e3b6c408df4b9be418201
pyporn-san <76444728+pyporn-san@users.noreply.github.com>
2020-12-25 22:12:34 +0330

    formatting

--- multporn/__init__.py
+++ multporn/__init__.py
@@ -1,2 +1,14 @@
 from .multporn import *
 __version__ = "0.0.4"
+"""
+Basic Usage
+>>> from Multporn import multporn
+>>> comic = Multporn("https://multporn.net/comics/between_friends")
+>>> comic.downloadImages()
+
+Basic Utils usage
+>>> from Multporn import multporn, Utils
+>>> results = Utils.search("zelda")
+>>> comic = Multporn(results[0])
+>>> comic.downloadImages()
+"""
