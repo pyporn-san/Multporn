bac043876fe7ab84ecbf2a1fd9ca0c6bcd68a868
pyporn-san <pypornsan@gmail.com>
2021-03-01 15:51:27 +0330

    updates multporn.py

--- multporn/multporn.py
+++ multporn/multporn.py
@@ -216,7 +216,7 @@ class Multporn(RequestHandler):
     def handler(self) -> RequestHandler:
         return self.__handler
 
-    def downloadContent(self, root: Path = Path("Albums/"), printProgress: bool = True):
+    def downloadContent(self, root: Path = Path("Albums"), printProgress: bool = True):
         """
         Downloads all comic pages that don't already exist in the directory
         logging can be disabled by passing false to printProgress
