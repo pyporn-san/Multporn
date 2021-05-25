54ba3692f79b42b6348c043799b6c3e3089a23a6
pyporn-san <pypornsan@gmail.com>
2021-05-25 20:26:11 +0430

    added functools to autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ release = '1.3.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
-napoleon_google_docstring = True
+extensions = ['sphinx.ext.autodoc']
+autodoc_mock_imports = ['functools']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
